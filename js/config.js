2c1f133227c48f3e5f598b72bff2c44a63a5919a
Christianity (maybe) <csanchez_msasd@magnoliascience.org>
2026-09-15 11:57:32 -0700

    modified friction

--- js/config.js
+++ js/config.js
@@ -21,7 +21,7 @@ var CONFIG = {
   MOVE_SPEED: 0,      // pixels per frame left and right
   MOVE_VELOCITY: 0.25,// amount of change in velocity speed
   MAX_VELOCITY: 10,   // maximum velocity speed
-  MOVE_FRICTION: 1.1, // amount velocity slows down when not moving
+  MOVE_FRICTION: 0.95,// % of velocity when not moving
   JUMP_POWER: 13,     // how hard the jump pushes UP. bigger = higher
   GRAVITY: 0.5,       // how hard the world pulls DOWN. bigger = heavier
   MAX_FALL: 16,       // fastest the player is allowed to fall
