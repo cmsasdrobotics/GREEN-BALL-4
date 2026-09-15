2af436cf7be8d07093da69dca946313a2d37a781
Christianity (maybe) <csanchez_msasd@magnoliascience.org>
2026-09-14 19:16:58 -0700

    modified jump

--- js/config.js
+++ js/config.js
@@ -22,8 +22,8 @@ var CONFIG = {
   MOVE_VELOCITY: 0.25,// amount of change in velocity speed
   MAX_VELOCITY: 10,   // maximum velocity speed
   MOVE_FRICTION: 1.1, // amount velocity slows down when not moving
-  JUMP_POWER: 10,     // how hard the jump pushes UP. bigger = higher
-  GRAVITY: 0.7,       // how hard the world pulls DOWN. bigger = heavier
+  JUMP_POWER: 12,     // how hard the jump pushes UP. bigger = higher
+  GRAVITY: 0.6,       // how hard the world pulls DOWN. bigger = heavier
   MAX_FALL: 16,       // fastest the player is allowed to fall
 
   // --- the player's size ----------------------------------------------
