97f1acd01f29a37d64ab49b999f387b3f34a0a5e
Christianity (maybe) <csanchez_msasd@magnoliascience.org>
2026-09-14 18:18:06 -0700

    small bug fix

--- js/config.js
+++ js/config.js
@@ -21,7 +21,7 @@ var CONFIG = {
   MOVE_SPEED: 0,      // pixels per frame left and right
   MOVE_VELOCITY: 0.5, // amount of change in velocity speed
   MAX_VELOCITY: 8,    // maximum velocity speed
-  MOVE_FRICTION: 1.5  // amount velocity slows down when not moving
+  MOVE_FRICTION: 1.5,  // amount velocity slows down when not moving
   JUMP_POWER: 15,     // how hard the jump pushes UP. bigger = higher
   GRAVITY: 0.8,       // how hard the world pulls DOWN. bigger = heavier
   MAX_FALL: 16,       // fastest the player is allowed to fall
