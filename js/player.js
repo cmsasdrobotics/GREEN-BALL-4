baabbd99baadf651c3b466d307f1b85bebae7169
Christianity (maybe) <csanchez_msasd@magnoliascience.org>
2026-09-14 19:03:25 -0700

    step x bug fix

--- js/player.js
+++ js/player.js
@@ -50,12 +50,11 @@ Player.update = function () {
 
   // --- 4. move sideways, one pixel at a time, stopping at walls -------
   var stepX = 0;
-  if (Player.vx > 0) { stepX = 0.01; }
-  if (Player.vx < 0) { stepX = -0.01; }
+  if (Player.vx > 0) { stepX = 1; }
+  if (Player.vx < 0) { stepX = -1; }
 
   for (var i = 0; i < Math.abs(Player.vx); i++) {
     if (Collide.hitsSolid(Player.x + stepX, Player.y, size, size)) { break; }
-    Player.x = Player.x + stepX;
     Player.angle = Player.angle + stepX / CONFIG.PLAYER_RADIUS; // roll it
   }
 
