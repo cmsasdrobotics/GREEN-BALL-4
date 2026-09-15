a93321158e62ee99f5c25d00c9c9492a244c1eff
Christianity (maybe) <csanchez_msasd@magnoliascience.org>
2026-09-14 17:53:55 -0700

    bug fixes on velocity

--- js/player.js
+++ js/player.js
@@ -32,11 +32,11 @@ Player.update = function () {
 
   // --- 1. decide how fast to go sideways ------------------------------
   Player.vx = CONFIG.MOVE_SPEED;
-  if (Input.left)  { CONFIG.MOVE_SPEED = CONFIG.MOVE_SPEED - 0.5; 
-  } else if (Input.right) { CONFIG.MOVE_SPEED = CONFIG.MOVE_SPEED + 0.5; 
-  } else { CONFIG.MOVE_SPEED = CONFIG.MOVE_SPEED / 2}
-  if (CONFIG.MOVE_SPEED > 5)  { CONFIG.MOVE_SPEED = 5; }
-  if (CONFIG.MOVE_SPEED < -5)  { CONFIG.MOVE_SPEED = -5; }
+  if (Input.left)  { CONFIG.MOVE_SPEED = CONFIG.MOVE_SPEED - CONFIG.MOVE_VELOCITY; 
+  } else if (Input.right) { CONFIG.MOVE_SPEED = CONFIG.MOVE_SPEED + CONFIG.MOVE_VELOCITY; 
+  } else { CONFIG.MOVE_SPEED = CONFIG.MOVE_SPEED / MOVE_FRICTION }
+  if (CONFIG.MOVE_SPEED > MAX_VELOCITY)  { CONFIG.MOVE_SPEED = MAX_VELOCITY; }
+  if (CONFIG.MOVE_SPEED < -MAX_VELOCITY)  { CONFIG.MOVE_SPEED = -MAX_VELOCITY; }
 
   // --- 2. jump, but only if we are standing on something --------------
   if (Input.jump && Player.onGround) {
