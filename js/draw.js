109ecfd44c5b4d22185a7cc67ae36defeede4760
Christianity (maybe) <csanchez_msasd@magnoliascience.org>
2026-09-17 12:09:17 -0700

    Adjust enemy head spike color and height

--- js/draw.js
+++ js/draw.js
@@ -143,12 +143,12 @@ Draw.enemies = function () {
 Draw.enemy = function (x, y, width, height) {
   var ctx = Draw.ctx;
   var spikeWidth = width * (2 / 3);
-  var spikeHeight = height * (1 / 2);
+  var spikeHeight = height * (1 / 3);
   var spikeLeft = x + (width - spikeWidth) / 2;
 
   // The spike sits on the enemy's head to show it cannot be squished.
-  ctx.fillStyle = "#e84b4b";
-  ctx.strokeStyle = "#8d2020";
+  ctx.fillStyle = "#b9bec5";
+  ctx.strokeStyle = "#6a7079";
   ctx.lineWidth = 3;
   ctx.beginPath();
   ctx.moveTo(spikeLeft, y + 3);
