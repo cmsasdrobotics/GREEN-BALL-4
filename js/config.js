6b4b8f2d31ef0e82b2a8c184231d9cebbeb5948b
Christianity (maybe) <csanchez_msasd@magnoliascience.org>
2026-09-18 11:48:11 -0700

    Adjust enemy hop and stomp bounce power values

--- js/config.js
+++ js/config.js
@@ -27,8 +27,8 @@ var CONFIG = {
   MAX_FALL: 16,       // fastest the player is allowed to fall
 
   // --- enemies --------------------------------------------------------
-  ENEMY2_HOP_POWER: 14,   // enemy 2's hop is higher than the other enemies
-  STOMP_BOUNCE_POWER: 9,  // player bounce after stomping an enemy
+  ENEMY2_HOP_POWER: 10,   // enemy 2's hop is higher than the other enemies
+  STOMP_BOUNCE_POWER: 5,  // player bounce after stomping an enemy
 
   // --- the player's size ----------------------------------------------
   PLAYER_SIZE: 32,    // the player collides as a 32x32 box
