15cc9833d626972b2121c2a698aa89f094f391bc
Christianity (maybe) <csanchez_msasd@magnoliascience.org>
2026-09-24 11:19:09 -0700

    Update level.js

--- js/level.js
+++ js/level.js
@@ -71,9 +71,7 @@ Level.build = function (levelNumber) {
 Level.resetEntities = function () {
   Level.ammoPickups = [];
   Level.enemies = [];
-  Level.pellets = [];
-  hopCooldown: 0,  
-  
+  Level.pellets = [];  
 
   for (var row = 0; row < CONFIG.ROWS; row++) {
     for (var col = 0; col < Level.cols; col++) {
